16267ea0251bfaa05789829923b08862a1ad9e59
sarahhae <helloitssarahhae@gmail.com>
2020-09-30 13:56:08 +1300

    UPDATE

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -6,37 +6,9 @@
 // board function: 9 boxes. Random picks. once clicked, return X or O.
 // Winning conditions. First up to 10 wins the whole game.
 // Three buttons for "Start Game", "Play Again", New Game.
-
-
-const players = {
-  firstPlayer: 1,
-  secondPlayer: 2,
-}
-
-// const clearGame = function () {
-//   if ( $('*.board')on.('click', function () === true )
-//   // pop up message with button to restart / play.
-// }
-
-$(document).ready( function () {
-clearBoard();
-let turn = 0;
-
-
-
-// GAME FUNCTION = get the board running first. Once clicked on a box, add the players icon ( X or O ).
-// function is : when a 'player' chooses a box out of 9, return ( X or O ). Give a class to the boxes chosen by each player.
-
-const player1 = function () {
-  $('#player1').addClass('cross');
-  $('.box').addClass('active');
-  })
-}
-
-
-// WINNING COMBO : PLAYER 1
-const checkWinner = function () {
-  let winner;
+// player 2: wins message.
+// draw message.
+// player 1 wins message.
 
 let winningCombo = { // 3 row win, 3 columns win, 2 diagonal win.
   win1: $('#1, #2, #3').length === 3, // first row across
@@ -48,21 +20,105 @@ let winningCombo = { // 3 row win, 3 columns win, 2 diagonal win.
   win7: $('#1, #5, #9').length === 3, // cross from #1 to #9
   win8: $('#3, #5, #7').length === 3, // cross from #3 to #7
 }
-console.log(winningCombo);
 
-// if player 1 has any of the winning combinations on the board, player 1 wins.
-// if player 2 has any of the winning combinations on the board, player 2 wins.
-
-let draw = (board === 9) && (!$('#player1')) && (!$('#player2'));
-console.log(draw);
-
-
+// GAME FUNCTION = get the board running first. Once clicked on a box, add the players icon ( X or O ).
+// function is : when a 'player' chooses a box out of 9, return ( X or O ). Give a class to the boxes chosen by each player.
+let turn = 1;
+// let firstPlayer = $('#player1');
+// let secondPlayer = $('#player2');
+let newGame = 0;
 
 
+$(document).ready( function () {
+  let turn = 1;
+  let click = 0; // click variable to count click numbers
+  let newGame = 0;
 
-// player 2: wins message.
-// draw message.
-// player 1 wins message.
+  $( ".box" ).one( 'click', function () { // added 'one' and works but can not restart or refresh the board. Make function. .unbind()?
+    click++ // calling call funtion each time a click has been made
+    if ( turn === 1 ) {
+      console.log("player 1");
+      turn = 2;
+      $(this).addClass('X').text('X');
+      checkWinner("X");
+    } else {
+      console.log("player 2");
+      turn = 1;
+      $(this).addClass('O').text('O');
+      checkWinner("O");
+    }
+  });
+
+  // WINNING COMBO : PLAYER 1
+  const checkWinner = function (symbol) {
+
+    if ($('#1').hasClass(symbol) &&
+    $('#2').hasClass(symbol) &&
+    $('#3').hasClass(symbol)) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#4').hasClass(symbol) &&
+    $('#5').hasClass(symbol) &&
+    $('#6').hasClass(symbol)) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#7').hasClass(symbol) &&
+    $('#8').hasClass(symbol) &&
+    $('#9').hasClass(symbol)) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#1').hasClass(symbol) &&
+    $('#4').hasClass(symbol) &&
+    $('#7').hasClass(symbol)) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#2').hasClass(symbol) &&
+    $('#5').hasClass(symbol) &&
+    $('#8').hasClass(symbol) ) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#3').hasClass(symbol) &&
+    $('#6').hasClass(symbol) &&
+    $('#9').hasClass(symbol) ) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#1').hasClass(symbol) &&
+    $('#5').hasClass(symbol) &&
+    $('#9').hasClass(symbol) ) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else if ($('#3').hasClass(symbol) &&
+    $('#5').hasClass(symbol) &&
+    $('#7').hasClass(symbol) ) {
+      console.log(`Player ${symbol} wins!`);
+      return true;
+
+    } else {
+      if ( click >= 9 ) {
+        console.log(`It's a draw!`)
+      }
+      return false;
+    }
+  };
+
+
+
+const restartGame = function ( restart ) {
+  newGame++;
+$( ".box" ).one( 'click', function () {
+  if ( click >= 9 ) {
+    $(this).unbind($(`button.newgame`));
+  }
+})
+}
+
 
 
 });
