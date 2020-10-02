ff8d20dcfa8e80ba1b3c0bba9b4db239774646f7
sarahhae <helloitssarahhae@gmail.com>
2020-10-02 14:46:45 +1300

    Final Friday Presentation

--- README.md
+++ README.md
@@ -1,5 +1,49 @@
 TIC-TAC-TOE
 =============
-INSTRUCTIONS TO THE GAME
----------------------
 
+## Summary
+
+Tic-tac-toe is the first individual project, where we implement HTML, CSS, Javascript and jQuery. The game proposal requires two players of noughts (O) and crosses (X) - or other player features. The aim of the game is for players to connect a row of three, within the nine available winning combinations by taking turns. The project outcome requires to create and map out the game logic, create sperate HTML, CSS, and js files, and craft a readme.md file. Technical requirements are to program the game, so the players switch turns, rendering a board for players to place their mark on the browser, visual display of keeping score, DOM manipulation on Javascript, and deploy the game online.
+
+## Key functionalities
+
+Two-player functionality to take turns.
+'Draw' functionality for when the game board meets full capacity with no winners.
+'Play' functionality to clear the board and continue the game count.
+'New Game' functionality to clear board/scoreboard and restart the full game.
+
+## Design Layout - CSS, HTML, Javascript, jQuery
+
+The aim for my overall layout was to design a simple yet aesthetically pleasing game. Tic-tac-toe is an easy and simple game, so my goal was to enhance the design for a new experience. I experimented with event-listeners and methods to draw attention to critical features such as enlarging buttons with hover effects, switching player icon colors and flipping game board as it resets. For visual aspect, I added colored box-shadows to create a 3D effect that pops out from a simple background render. During the process, I stumbled upon the difficulties of keeping the elements in place. See images below...
+
+![on large external screen](images/first-cut.jpg)
+
+![On macbook 13inch screen](images/second-cut.jpg)
+
+The bug can be resolved with a `max-width` in the css container class.
+
+## Functionality and Features
+
+* Clicks on players turn places player icon.
+* Box click disables once a box is taken.
+* Board is disabled once a player wins a round by matching a winning combination.
+* Winner's score or draw count displayed on score-board.
+* Play button clears board for new round.
+* New game button resets full game - scoreboard back to 0 and clear board.
+* Play and New Game button clears board with a flip-box functionality
+
+## BUGS, BUGS, MORE BUGS
+
+I stumbled upon a lot of bugs on the way for creating this exciting, fun amazing game... The main a repeated problem was repeating a lot of lines and not using methods in the correct manner. As an example of the many, I had used ```.addclass('flip-board')``` which only worked once. Another issue with this function was, I had a repeating function for ```$('#newgame').on('click', function ()``` which did not allow me to continuously flip the board once the button had been clicked. This was resolved by using the ```.toggleClass()``` method and by deleting the repeated function. See below :
+
+```  
+    $('#newgame').on('click', function () {
+    $('.box').removeClass('X O').empty();
+    $('#main-gameboard').toggleClass('flip-board');
+```
+
+To be continued with more bugs I found along the way...
+
+## Future Improvements
+
+Keeping my code DRY is the key skill for me to practice more often. Moving forward, I aim to continue on with this project as I get better with Javascript, jQuery and CSS. Also, naming variables in a proper manner is essential and something I need to work on more as I progress through the course.
--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,17 +1,3 @@
-// player 1 wins message.
-// player 2: wins message.
-// draw message.
-// scoreboard function. Goes up by 1 every time a player wins or the 'draw'
-// score goes up by 1 once every turn is drawn.
-// board function: 9 boxes. Random picks. once clicked, return X or O.
-// Winning conditions. First up to 10 wins the whole game.
-// Three buttons for "Start Game", "Play Again", New Game.
-// player 2: wins message.
-// draw message.
-// player 1 wins message.
-
-// GAME FUNCTION = get the board running first. Once clicked on a box, add the players icon ( X or O ).
-// function is : when a 'player' chooses a box out of 9, return ( X or O ). Give a class to the boxes chosen by each player.
 let turn = 1;
 let gameCount = 0;
 let click = 0;
@@ -26,7 +12,7 @@ $(document).ready( function () {
 
   $('#playgame').on('click', function () {
     $('.box').removeClass('X O').empty();
-    $('#main-gameboard').on('click').toggleClass('flip-board');
+    $('#main-gameboard').on('click').toggleClass('flip-board'); // .toggleClass for continuous flips one button is clicked.
     click = 0;
     winningPlayer = undefined;
   });
@@ -43,74 +29,41 @@ $(document).ready( function () {
     winningPlayer = undefined;
   });
 
-  // -----------------WINNING MESSAGE---------------//
-  //
-  // let player1count = 0;
-  // const player1win = function () {
-  //   $('.player1win').addClass('show');
-  //   $('.box').addClass('X');
-  //   player1count += 1
-  //   $('#player1-score').text(player1count);
-  //   $('.box').unbind();
-  //
-  //   $('.player1win').on('click', function (){
-  //     $('.player1win').removeClass('show');
-  //     $('.box').removeClass('X');
-  //     //call reset function ie reset();
-  //   });
-  //   return true;
-  // };
-  //
-  // let player2count = 0;
-  // const player2win = function () {
-  //   $('.player2win').addClass('show');
-  //   $('.box').addClass('O');
-  //   player1count += 1
-  //   $('#player2-score').text(player1count);
-  //   $('.box').unbind();
-  //
-  //   $('.player2win').on('click', function (){
-  //     $('.player2win').removeClass('show');
-  //     $('.box').removeClass('O');
-  //     //call reset function ie reset();
-  //   });
-  //   return true;
-  // };
-  //
-
   // ---------------------SCOREBOARD COUNTER------------------------//
 
   const scoreBoard = function ( score ) {
-    let player1Wins = $('#player1-score')
-    let player2Wins = $('#player2-score')
-    let drawCount = $('#noplayer-score')
+    let player1Wins = $('#player1-score');
+    let player2Wins = $('#player2-score');
+    let drawCount = $('#noplayer-score');
+
     if ( score === 'X' ) {
-      player1Wins.html(`Player 1 : ${player1count}`)
+      player1Wins.html(`Player 1 : ${player1count}`);
+
     } else if ( score === 'O' ) {
-      player2Wins.html(`Player 2 : ${player2count}`)
+      player2Wins.html(`Player 2 : ${player2count}`);
     }
   };
 
   // -----------------------BOARDGAME FUNCTION----------------------//
 
   $( ".box" ).on( 'click', function () {
-    if (!$(this).hasClass('X') && !$(this).hasClass('O') && winningPlayer === undefined ) {
-      click++ // adding each time a click has been made
-      console.log(click);
+    if (!$(this).hasClass('X') && !$(this).hasClass('O') && winningPlayer === undefined ) { // checks if a box is vacant, if not, places their marker in it and check for win or draw.
+      click++ // Adding player clicks
+
       if ( click >= 9 ) {
         let draw = $('#noplayer-score')
         drawCount += 1;
         draw.html(`Draw : ${drawCount}`)
       }
+
       if ( turn === 1 ) {
-        console.log("player 1");
         turn = 2;
         $(this).addClass('X').text('X');
         $('#player1').css({'backgroundColor': 'lightgreen', 'color': 'white'});
         $('#player2').css({'backgroundColor': 'black', 'color': 'white'});
         checkWinner("X");
+
       } else {
-        console.log("player 2");
         turn = 1;
         $(this).addClass('O').text('O');
         $('#player2').css({'backgroundColor': 'lightgreen', 'color': 'white'});
@@ -130,13 +83,11 @@ $(document).ready( function () {
       winningPlayer = `${symbol}`;
       if ( symbol === 'X' ) {
         player1count += 1;
-        scoreBoard('X')
+        scoreBoard('X');
       } else if ( symbol === 'O' ) {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#4').hasClass(symbol) &&
@@ -145,28 +96,26 @@ $(document).ready( function () {
       winningPlayer = `${symbol}`
       if ( symbol === 'X' ) {
         player1count += 1;
-        scoreBoard('X')
+        scoreBoard('X');
       } else if ( symbol === 'O' ) {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#7').hasClass(symbol) &&
     $('#8').hasClass(symbol) &&
     $('#9').hasClass(symbol)) {
       winningPlayer = `${symbol}`
+
       if ( symbol === 'X' ) {
         player1count += 1;
-        scoreBoard('X')
+        scoreBoard('X');
+
       } else if ( symbol === 'O' ) {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#1').hasClass(symbol) &&
@@ -180,8 +129,6 @@ $(document).ready( function () {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#2').hasClass(symbol) &&
@@ -195,8 +142,6 @@ $(document).ready( function () {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#3').hasClass(symbol) &&
@@ -210,8 +155,6 @@ $(document).ready( function () {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#1').hasClass(symbol) &&
@@ -225,8 +168,6 @@ $(document).ready( function () {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else if ($('#3').hasClass(symbol) &&
@@ -240,13 +181,10 @@ $(document).ready( function () {
         player2count += 1;
         scoreBoard('O');
       }
-
-      console.log(`Player ${symbol} wins!`);
       return true;
 
     } else {
       if ( click >= 9 ) {
-        console.log(`It's a draw!`)
       }
       return false;
     }
